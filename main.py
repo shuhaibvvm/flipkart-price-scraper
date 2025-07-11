8ff943ce1dcddf5ebc5674db0793121d958bd1e7
suhaib <sshuhaib274@gmail.com>
2025-07-11 16:06:34 +0530

    scraped all listings by clciking next button

--- main.py
+++ main.py
@@ -5,32 +5,64 @@ import pandas as pd
 
 async def scrape_prices():
     async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=False)
+        scraping = True  
+        browser = await p.chromium.launch(headless=True)
         page = await browser.new_page()
         
+        page_number = 1
+            
         await page.goto("https://www.flipkart.com/search?q=laptop", timeout=60000) 
         await page.wait_for_selector("div:has-text('Chromebook')") #Playwright pseudo-class extension
-        
-        #This XPath selector:
-        titles = page.locator("//div[contains(text(),'Chromebook') or contains(text(),'Laptop') or contains(text(),'ASUS') or contains(text(),'Lenovo')]")
-        prices = page.locator("//div[contains(text(),'₹')]")
-        
-        count = await titles.count()
         data = []
+
         
-        for i in range(count):
-            title = await titles.nth(i).text_content()
-            price = await prices.nth(i).text_content()
+        while scraping: 
+            #This XPath selector:
+            titles = page.locator("//div[contains(text(),'Chromebook') or contains(text(),'Laptop') or contains(text(),'ASUS') or contains(text(),'Lenovo')]")
+            prices = page.locator("//div[contains(text(),'₹')]")
             
-            if title and price:
-                data.append(
-                    {
-                        "title": title.strip(),
-                        "price": price.strip().replace("₹","").replace(",","")
-                    }
-                )
+            count = await titles.count()
+            
+            for i in range(count):
+                title = await titles.nth(i).text_content()
+                try:
+                    price = await prices.nth(i).text_content()
+                except:
+                    print(f"Price not found for item{i}")
+                    continue
+                
+                if title and price:
+                    data.append(
+                        {
+                            "title": title.strip(),
+                            "price": price.strip().replace("₹","").replace(",","")
+                        }
+                    )
+            next_locater = page.locator("span:has-text('Next')")
+            count = await next_locater.count()
+            if count>0:
+                # save the title before clcik
+                first_title_before = await titles.first.text_content()
+                await next_locater.click()
+                await page.wait_for_timeout(3000)  # give time to load next page
+                
+                # Refresh titles after click
+                titles = page.locator("//div[contains(text(),'Chromebook') or contains(text(),'Laptop') or contains(text(),'ASUS') or contains(text(),'Lenovo')]")
+                first_title_after = await titles.first.text_content()
+                
+                if first_title_before == first_title_after:
+                    print("No Change after clicking next - last page reached")
+                    break
+                else:
+                    page_number += 1
+                    print(f"Page number {page_number}")
+                
+            else:
+                print("Next button not found -> Ending Scraping")
+                break
         await browser.close()
         return pd.DataFrame(data)
+        
     
     
 # Step_2 Clean the Data
